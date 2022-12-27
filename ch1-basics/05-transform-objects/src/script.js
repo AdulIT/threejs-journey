ea7643a51cda0564ee6b987e17885d3ee9aef61f
AdulIt <spilberg.max@bk.ru>
2022-12-28 00:04:52 +0600

    feat: Add 3 cubes in groups, position, rotatioin and scale

--- ch1-basics/05-transform-objects/src/script.js
+++ ch1-basics/05-transform-objects/src/script.js
@@ -10,14 +10,59 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 1.5
+group.rotation.z = 3.14159 * 0.25
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = 2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = - 2
+group.add(cube3)
+// Position
+// mesh.position.x = 0.7
+// mesh.position.y = - 0.6
+// mesh.position.z = 1
+// mesh.position.set(0.7, - 0.6, 1)
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+// mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+// Math.PI - it's equal to 3.14159
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+// mesh.position.normalize()
+// console.log(mesh.position.length())
 
 /**
  * Sizes
- */
+//  */
 const sizes = {
     width: 800,
     height: 600
@@ -29,6 +74,12 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+// camera.lookAt(mesh.position)
+
+// Distance to
+// Distance between the mesh and camera(from mesh to camera)
+// console.log(mesh.position.distanceTo(camera.position))
 
 /**
  * Renderer
