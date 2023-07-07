022fc50c245d1bb0d2cdf3d85d5c791332857076
AdulIT <spilberg.max@bk.ru>
2023-07-07 10:25:01 +0600

    hamburger model added

--- ch1-basics/03-basic-scene/script.js
+++ ch1-basics/03-basic-scene/script.js
@@ -1,10 +1,12 @@
-// Scene
+
 const scene = new THREE.Scene()
 
 // Red cube
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+
 const mesh = new THREE.Mesh(geometry, material)
+
 scene.add(mesh)
 
 // Sizes
@@ -15,19 +17,14 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 4
-camera.position.x = 2
-camera.position.y = 2
+camera.position.z = 3
 scene.add(camera)
 
-// Canvas
-
-
 // Renderer
-const canvas = document.querySelector('.webgl')
+const canvas = document.querySelector('canvas.webgl')
 const renderer = new THREE.WebGLRenderer({
     canvas
 })
-renderer.setSize(sizes.width, sizes.height)
 
+renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
--- ch3-advanced-techniques /23-raycaster-and-mouse-events/src/script.js
+++ ch3-advanced-techniques /23-raycaster-and-mouse-events/src/script.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { Vector3 } from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
 
 /**
  * Base
@@ -13,6 +16,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
 
 /**
  * Objects
@@ -35,6 +39,23 @@ const object3 = new THREE.Mesh(
 object3.position.x = 2
 
 scene.add(object1, object2, object3)
+
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster()
+
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0)
+// rayDirection.normalize()
+
+// raycaster.set(rayOrigin, rayDirection)
+
+// const intersect = raycaster.intersectObject(object2)
+// console.log(intersect)
+
+// const intersects = raycaster.intersectObjects([object1, object2, object3])
+// console.log(intersects)
 
 /**
  * Sizes
@@ -60,6 +81,38 @@ window.addEventListener('resize', () =>
 })
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) =>
+{
+    mouse.x = event.clientX / sizes.width * 2 - 1
+    mouse.y = - (event.clientY / sizes.height) * 2 + 1
+})
+
+window.addEventListener('click', (event) =>
+{
+    if (currentIntersect)
+    {
+        switch (currentIntersect.object)
+        {
+            case object1:
+                console.log('click on a sphere 1')
+                break
+
+            case object2:
+                console.log('click on a sphere 2')
+                break
+                
+            case object3:
+                console.log('click on a sphere 3')
+                break
+        }
+    }
+})
+
+/**
  * Camera
  */
 // Base camera
@@ -81,13 +134,102 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+
+let model = null
+
+gltfLoader.load(
+    './models/Duck/glTF-Binary/Duck.glb',
+    (gltf) =>
+    {
+        model = gltf.scene
+        model.position.y = - 1.2
+        scene.add(model)
+    }
+)
+
+/**
+ * Lights
+ */
+
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.3)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight('#ffffff', 0.7)
+directionalLight.position.set(1, 2, 3)
+scene.add(directionalLight)
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
 
+let currentIntersect = null
+
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+
+    // Cast a ray
+    raycaster.setFromCamera(mouse, camera)
+
+    // const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0)
+    // rayDirection.normalize()
+
+    // raycaster.set(rayOrigin, rayDirection)
+
+    const objectsToTest = [object1, object2, object3]
+    const intersects = raycaster.intersectObjects(objectsToTest)
+
+    for (const object of objectsToTest)
+    {
+        object.material.color.set('#ff0000')
+    }
+
+    for (const intersect of intersects)
+    {
+        intersect.object.material.color.set('#0000ff')
+    }
+
+    if (intersects.length)
+    {
+        if (currentIntersect === null)
+        {
+            console.log('mouse enter')
+        }
+        currentIntersect = intersects[0]
+    }
+    else
+    {
+        if (currentIntersect)
+        {
+            console.log('mouse leave')
+        }
+        currentIntersect = null
+    }
+
+    // Test intersect wuth model
+    if (model)
+    {
+        const modelIntersects = raycaster.intersectObject(model)
+
+        if (modelIntersects.length)
+        {
+            model.scale.set(1.2, 1.2, 1.2)
+        }
+        else
+        {
+            model.scale.set(1, 1, 1)
+        }
+    }
 
     // Update controls
     controls.update()
